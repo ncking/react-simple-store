df38ad8f24b9f555b3ecfa329693473d85e1480a
Nigel King <nigel.c.king@gmail.com>
2023-05-19 08:21:29 +0100

    stash

---
+++ test/vanilla/subscribe.test.ts
@@ -0,0 +1,53 @@
+import { recreateCounterStore } from "../fixtures";
+
+let subscribe;
+let setState;
+beforeEach(() => {
+  ({ subscribe, setState } = recreateCounterStore());
+});
+
+describe("subscribe()", () => {
+  it("should be called on store change", () => {
+    const spy = jest.fn();
+    subscribe(spy);
+    setState({ dummy: Date.now() });
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should be called on slice change", () => {
+    const spy = jest.fn();
+    subscribe((s) => s.dummy, spy);
+    setState({ dummy: Date.now() });
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should not be called on other slice change", () => {
+    const spy = jest.fn();
+    subscribe((s) => s.dummy, spy);
+    setState({ dummy2: Date.now() });
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should not be called if equality true", () => {
+    const spy = jest.fn();
+    subscribe(
+      (s) => s.dummy,
+      spy,
+      () => true
+    );
+    setState({ dummy: Date.now() });
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should be called if equality false", () => {
+    const spy = jest.fn();
+    setState({ dummy: { name: "my object", created: Date.now() } });
+    subscribe(
+      (s) => s.dummy,
+      spy,
+      () => false
+    );
+    setState({ dummy2: Date.now() });
+    expect(spy).toHaveBeenCalled();
+  });
+});
