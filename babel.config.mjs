27bca767efdf9eafffc25224738c9beac606e5b7
Nigel King <nigel.c.king@gmail.com>
2023-05-23 16:13:42 +0100

    format

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,21 +1,12 @@
-import {resolve} from 'node:path'
 const plugins = [
     '@babel/transform-react-jsx',
-    ['babel-plugin-module-resolver', {
-        root: ['.'],
-        alias : {
-            "@createStore": resolve('./src/index.ts'),
-            "@shallowEqual": resolve('./src/shallow-equal.ts')
-        },
-    }],
 ]
-
-
 
 const presets = [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
   ]
+
 
 export default {
     presets,
