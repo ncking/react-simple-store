bb86572d0a532a91c8d7f526c81aec76b3540bb2
Nigel King <nigel.c.king@gmail.com>
2023-05-23 08:03:09 +0100

    stash

--- src/create-store-redux.ts
+++ src/create-store-redux.ts
@@ -3,8 +3,18 @@ import { State, SetState, GetState, Reducer, Action } from "./types";
 
 export const createStoreRedux = (reducer: Reducer, initialState: State) =>
   createStore(
-    (setState: SetState, getState: GetState) => ({
+    (setState: SetState, getState: GetState, { subscribe }) => ({
       dispatch: (action: Action) => setState(reducer(getState(), action)),
+      connect: (mapStateToProps, mapDispatchToProps) =>
+        subscribe(
+          mapStateToProps,
+          (state) => (component: any) => (props) =>
+            React.createElement(component, {
+              ...props,
+              ...state,
+              ...mapDispatchToProps(),
+            })
+        ),
     }),
     initialState
   );
