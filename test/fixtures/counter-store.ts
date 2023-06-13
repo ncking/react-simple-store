f6c09ea37cf9686d2436072785ec96a63144d674
Nigel King <nigel.c.king@gmail.com>
2023-06-13 15:00:57 +0100

    stash

--- test/fixtures/counter-store.ts
+++ test/fixtures/counter-store.ts
@@ -1,13 +1,15 @@
 import { createStore } from "../../src";
-const state = {
+export { createStore } from '../../src'
+
+export const state = {
   count: 0,
 };
 
-const actions = (set, get) => {
+export const actions = (set, get) => {
   return {
     increase: () => set({ count: ++get().count }),
     decrease: () => set({ count: --get().count }),
   };
 };
-const args = {actions, state}
+const args = { actions, state }
 export const recreateCounterStore = () => createStore(args);
