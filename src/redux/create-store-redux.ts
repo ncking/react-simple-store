e06f466e422a3dbebc6c041398c3021155a189cd
Nigel King <nigel.c.king@gmail.com>
2024-05-11 12:59:22 +0100

    stash

--- src/redux/create-store-redux.ts
+++ src/redux/create-store-redux.ts
@@ -1,4 +1,4 @@
 import { createStore } from "@raiz/react-simple-store";
 import type { Reducer, Action } from "./types";
-import { State, SetState, GetState } from  '../core/types';
+import { State, SetState, GetState } from  '@raiz/react-simple-store';
 export const createStoreRedux = (reducer: Reducer, state: State) => createStore({ actions: (setState: SetState, getState: GetState) => ({ dispatch: (action: Action) => setState(reducer(getState(), action)) }), state });
